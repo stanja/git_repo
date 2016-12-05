e76b72edb6be693b40429a32f947a18617954e71
stanja <tanja.sekulic@comtrade.com>
2016-12-05 15:52:20 +0100

    lesson [#5]HTTP

--- AngularApps/ng-cribs/cribsFactory.js
+++ AngularApps/ng-cribs/cribsFactory.js
@@ -1,30 +1,11 @@
 angular
     .module('ngCribs')
-    .factory('cribsFactory', function(){
-       var cribsData =  [
-            {
-                "type": "Condo",
-                "price": 220000, 
-                "address": "213 Grove Street",
-                "description": "Excellent place, realy nice view!"
-            },
-            {
-                "type": "House",
-                "price": 410500, 
-                "address": "7823 Winding Way",
-                "description": "Beautiful home with lots of space for a large family"
-            },
-            {
-                "type": "Duplex",
-                "price": 395000, 
-                "address": "834 River Lane",
-                "description": "Great neighbourhood and lot's of nice green space"
-            }
-        ];
-
+    .factory('cribsFactory', function($http) {
+       
         function getCribs() {
-            return cribsData;
+            return $http.get('data/data.json');
         }
+
         return {
             getCribs: getCribs
         }
--- AngularApps/ng-cribs/scripts/cribsController.js
+++ AngularApps/ng-cribs/scripts/cribsController.js
@@ -1,5 +1,10 @@
 angular
     .module('ngCribs')
     .controller('cribsController', function ($scope, cribsFactory) {
-        $scope.cribs = cribsFactory.getCribs();
+        $scope.cribs;
+        cribsFactory.getCribs().success(function(data) {
+            $scope.cribs = data;
+        }).error(function(error) {
+            console.log(error);
+        });
     });
