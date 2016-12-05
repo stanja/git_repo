df22a4df05d87a091a396d17c651e4e87d059012
stanja <tanja.sekulic@comtrade.com>
2016-12-05 16:25:59 +0100

    AngularApps

---
+++ AngularApps/ng-cribs/scripts/cribsController.js
@@ -0,0 +1,10 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function ($scope, cribsFactory) {
+        $scope.cribs;
+        cribsFactory.getCribs().success(function(data) {
+            $scope.cribs = data;
+        }).error(function(error) {
+            console.log(error);
+        });
+    });
