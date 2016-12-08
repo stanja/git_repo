b787c1da4b694ce3e05da385bb17fc3127f32e7c
stanja <tanja.sekulic@comtrade.com>
2016-12-08 16:42:28 +0100

    Finished tutorial, create "Real Estate Listings" web page

--- AngularApps/ng-cribs/scripts/cribsController.js
+++ AngularApps/ng-cribs/scripts/cribsController.js
@@ -2,18 +2,49 @@ angular
     .module('ngCribs')
     .controller('cribsController', ['$scope', 'cribsFactory', function ($scope, cribsFactory) {
 
-       /* var self = this;
-        self.cribsFactory = {};*/
-
+        $scope.cribs;
         cribsFactory.getCribs()
             .then(function (response) {
-                console.log(response.data);
                 $scope.cribs = (response.data);
                 console.log($scope.cribs);
             }, function (error) {
                 console.log(error);
             })
+        /* inicijalizovali smo vrednosti za min i max priceInfo kako nam stranica ne bi bila prazna dok ne izaberemo vrednosti u drop baru*/
+        $scope.priceInfo = {
+            min: 0,
+            max: 1000000
+        }
 
+        /* addCrib function - za dodavanje nove nekretnine na web stranici*/
+        $scope.newListing = {};
+
+        $scope.addCrib = function (newListing) {
+            newListing.image = 'default-crib';
+            $scope.cribs.push(newListing);
+            $scope.newListing = {};
+        }
+
+        /* editCrib function - za editovanje postojecih podataka u nekretninama*/
+        $scope.existingListing = {};
+
+        $scope.editCrib = function (crib) {
+            $scope.editListing = true;
+            $scope.existingListing = crib;
+        }
+
+        /* saveCribEdit function - snimanje editovanih u postojecim nekretninama na buttonu Save*/
+        $scope.saveCribEdit = function () {
+            $scope.existingListing = {};
+            $scope.editListing = false;
+        }
+        /* deleteCribEdit function - brisanje citave nekretne */
+        $scope.deleteCribEdit = function (listing) {
+             var index = $scope.cribs.indexOf(listing);
+             $scope.cribs.splice(index, 1);
+             $scope.existingListing = {};
+             $scope.editListing = false;
+        }
     }]);
 
 
--- AngularApps/ng-cribs/scripts/cribsFactory.js
+++ AngularApps/ng-cribs/scripts/cribsFactory.js
@@ -2,10 +2,17 @@ angular
     .module('ngCribs')
     .factory('cribsFactory', ['$http', function ($http) {
 
-        var cribsFactory = {};
-        cribsFactory.getCribs = function () {
+        /*  var cribsFactory = {};
+           cribsFactory.getCribs = function () {
+               return $http.get('http://localhost:8000/data/data.json')
+           }
+           return cribsFactory;*/
+
+        function getCribs() {
             return $http.get('http://localhost:8000/data/data.json')
         }
-        return cribsFactory;
+        return {
+            getCribs: getCribs
+        };
 
     }]);
